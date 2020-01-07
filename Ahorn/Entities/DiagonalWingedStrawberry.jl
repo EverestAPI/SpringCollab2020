03aa54fa9a370eab1710d4de439d6e8982dc01cf
WoofWoofDoggo <barkbarkpupper@gmail.com>
2020-01-07 18:48:02 -0500

    Basic files
    Still WIP

---
+++ Ahorn/Entities/DiagonalWingedStrawberry.jl
@@ -0,0 +1,25 @@
+﻿module SpringCollab2020DiagonalWingedStrawberry
+using ..Ahorn, Maple
+@mapdef Entity "SpringCollab2020/diagonalWingedStrawberry" DiagonalWingedStrawberry(x::Integer, y::Integer)
+
+sprite = "collectables/strawberry/wings01"
+
+const placements = Ahorn.PlacementDict(
+	"Diagonal Winged Strawberry (Spring Collab 2020)" => Ahorn.EntityPlacement(
+		DiagonalWingedStrawberry
+	)
+)
+
+function Ahorn.selection(entity::DiagonalWingedStrawberry)
+	x, y = Ahorn.position(entity)
+
+	return Ahorn.Rectangle[Ahorn.getSpriteRectangle(sprite, x, y)]
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::DiagonalWingedStrawberry, room::Maple.Room)
+	x, y = Ahorn.position(entity)
+
+	Ahorn.drawImage(ctx, sprite, x, y)
+end
+
+end
---
+++ DiagonalWingedStrawberry.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.SpringCollab2020 {
+    class DiagonalWingedStrawberry : Strawberry {
+        public DiagonalWingedStrawberry(EntityData data, Vector2 offset, EntityID gid) : base(data, offset, gid) {
+            Component[] componentArray = Components.ToArray();
+
+            foreach(Component comp in componentArray) {
+                if (comp is DashListener)
+                    Components.Remove(comp);
+            }
+        }
+
+        private void OnDiagDash(Vector2 dir) {
+            Console.WriteLine("Diagonal Dash OnDiagDash : Vector is "+dir.X+" "+dir.Y);
+        }
+
+        private bool flyingAway;
+    }
+}
