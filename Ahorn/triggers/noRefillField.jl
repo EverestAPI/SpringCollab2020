dae5771b736f5cc6c785593959a96a25e157d1f7
max480 <max480@free.fr>
2020-01-07 23:09:41 +0100

    Delete the parameter, and handle multiple triggers side to side instead

--- Ahorn/triggers/noRefillField.jl
+++ Ahorn/triggers/noRefillField.jl
@@ -2,7 +2,7 @@ module SpringCollab2020NoRefillField
 
 using ..Ahorn, Maple
 
-@mapdef Trigger "SpringCollab2020/NoRefillField" NoRefillField(x::Integer, y::Integer, width::Integer=16, height::Integer=16, noGroundRefillInside::Bool=true)
+@mapdef Trigger "SpringCollab2020/NoRefillField" NoRefillField(x::Integer, y::Integer, width::Integer=16, height::Integer=16)
 
 const placements = Ahorn.PlacementDict(
     "No Refill Field (SpringCollab2020)" => Ahorn.EntityPlacement(
