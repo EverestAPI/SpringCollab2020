70f9f115eef474e57184f3aef6a9f15372670a75
max480 <max480@free.fr>
2020-01-08 08:25:11 +0100

    Fix Ahorn plugin + Restore refills if saving & quitting in trigger

--- Ahorn/triggers/noRefillField.jl
+++ Ahorn/triggers/noRefillField.jl
@@ -2,10 +2,10 @@ module SpringCollab2020NoRefillField
 
 using ..Ahorn, Maple
 
-@mapdef Trigger "SpringCollab2020/NoRefillField" NoRefillField(x::Integer, y::Integer, width::Integer=16, height::Integer=16)
+@mapdef Trigger "SpringCollab2020/NoRefillField" NoRefillField(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight)
 
 const placements = Ahorn.PlacementDict(
-    "No Refill Field (SpringCollab2020)" => Ahorn.EntityPlacement(
+    "No Refill Field (Spring Collab 2020)" => Ahorn.EntityPlacement(
         NoRefillField,
         "rectangle",
     ),
--- SpringCollab2020Module.cs
+++ SpringCollab2020Module.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Celeste.Mod.SpringCollab2020.Triggers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,9 +15,21 @@ namespace Celeste.Mod.SpringCollab2020 {
         }
 
         public override void Load() {
+            Everest.Events.Level.OnExit += onLevelExit;
         }
 
         public override void Unload() {
+            Everest.Events.Level.OnExit -= onLevelExit;
+        }
+
+        private void onLevelExit(Level level, LevelExit exit, LevelExit.Mode mode, Session session, HiresSnow snow) {
+            if (mode == LevelExit.Mode.SaveAndQuit) {
+                // if saving in a No Refill Field, be sure to restore the refills.
+                Player player = level.Tracker.GetEntity<Player>();
+                if (player != null && player.CollideCheck<NoRefillField>()) {
+                    level.Session.Inventory.NoRefills = false;
+                }
+            }
         }
     }
 }
