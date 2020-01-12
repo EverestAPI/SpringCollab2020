a8ad060bfee8513ef7d5c84a9c291270775d675c
PenguinOwl <henryjboles@gmail.com>
2020-01-11 17:09:40 -0800

    Adds a crumble block variant that allows you to configure the timer that it takes for the blocks to fall down.

---
+++ Ahorn/entities/variableCrumble.jl
@@ -0,0 +1,55 @@
+module SpringCollab2020VariableCrumbleBlock
+
+using ..Ahorn, Maple
+
+@mapdef Entity "SpringCollab2020/variableCrumbleBlock" VariableCrumbleBlock(x::Integer, y::Integer, width::Integer=Maple.defaultBlockWidth, texture::String="default", timer::Number=0.4)
+
+const placements = Ahorn.PlacementDict(
+    "Variable Crumble Blocks ($(uppercasefirst(texture)))" => Ahorn.EntityPlacement(
+        VariableCrumbleBlock,
+        "rectangle",
+        Dict{String, Any}(
+            "texture" => texture
+        )
+    ) for texture in Maple.crumble_block_textures
+)
+
+Ahorn.editingOptions(entity::VariableCrumbleBlock) = Dict{String, Any}(
+    "texture" => Maple.crumble_block_textures
+)
+
+Ahorn.minimumSize(entity::VariableCrumbleBlock) = 8, 0
+Ahorn.resizable(entity::VariableCrumbleBlock) = true, false
+
+function Ahorn.selection(entity::VariableCrumbleBlock)
+    x, y = Ahorn.position(entity)
+    width = Int(get(entity.data, "width", 8))
+
+    return Ahorn.Rectangle(x, y, width, 8)
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::VariableCrumbleBlock, room::Maple.Room)
+    texture = get(entity.data, "texture", "default")
+    texture = "objects/crumbleBlock/$texture"
+    println(texture)
+
+    # Values need to be system specific integer
+    x = Int(get(entity.data, "x", 0))
+    y = Int(get(entity.data, "y", 0))
+
+    width = Int(get(entity.data, "width", 8))
+    tilesWidth = div(width, 8)
+
+    Ahorn.Cairo.save(ctx)
+
+    Ahorn.rectangle(ctx, 0, 0, width, 8)
+    Ahorn.clip(ctx)
+
+    for i in 0:ceil(Int, tilesWidth / 4)
+        Ahorn.drawImage(ctx, texture, 32 * i, 0, 0, 0, 32, 8)
+    end
+
+    Ahorn.restore(ctx)
+end
+
+end
---
+++ Entities/VariableCrumbleBlock.cs
@@ -0,0 +1,225 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.SpringCollab2020.Entities {
+    [CustomEntity("SpringCollab2020/variableCrumbleBlock")]
+    public class VariableCrumblePlatform : Solid {
+        public static ParticleType P_Crumble = CrumblePlatform.P_Crumble;
+
+        private List<Image> images;
+
+        private List<Image> outline;
+
+        private List<Coroutine> falls;
+
+        private List<int> fallOrder;
+
+        private ShakerList shaker;
+
+        private LightOcclude occluder;
+
+        private Coroutine outlineFader;
+
+        public float crumbleTime = 0.4f;
+
+        public string OverrideTexture;
+
+        public VariableCrumblePlatform(Vector2 position, float width, float timer)
+            : base(position, width, 8f, false) {
+            EnableAssistModeChecks = false;
+            crumbleTime = timer;
+        }
+
+        public VariableCrumblePlatform(EntityData data, Vector2 offset)
+            : this(data.Position + offset, (float) data.Width, data.Float("timer", 0.4f)) {
+        }
+
+        public override void Added(Scene scene) {
+            AreaData areaData = AreaData.Get(scene);
+            string crumbleBlock = areaData.CrumbleBlock;
+            if (OverrideTexture != null) {
+                areaData.CrumbleBlock = OverrideTexture;
+            }
+            orig_Added(scene);
+            areaData.CrumbleBlock = crumbleBlock;
+        }
+
+        private IEnumerator Sequence() {
+            while (true) {
+                Player player = GetPlayerOnTop();
+                bool onTop;
+                if (player != null) {
+                    onTop = true;
+                    Input.Rumble(RumbleStrength.Medium, RumbleLength.Medium);
+                } else {
+                    player = GetPlayerClimbing();
+                    if (player == null) {
+                        yield return (object) null;
+                        continue;
+                    }
+                    onTop = false;
+                    Input.Rumble(RumbleStrength.Medium, RumbleLength.Medium);
+                }
+                Audio.Play("event:/game/general/platform_disintegrate", Center);
+                shaker.ShakeFor(onTop ? crumbleTime + 0.1f : crumbleTime + 0.2f, false);
+                List<Image>.Enumerator enumerator = images.GetEnumerator();
+                try {
+                    while (enumerator.MoveNext()) {
+                        Image img2 = enumerator.Current;
+                        SceneAs<Level>().Particles.Emit(P_Crumble, 2, Position + img2.Position + new Vector2(0f, 2f), Vector2.One * 3f);
+                    }
+                } finally {
+                    ((IDisposable) enumerator).Dispose();
+                }
+                enumerator = default(List<Image>.Enumerator);
+                for (int m = 0; m < (onTop ? 1 : 3); m++) {
+                    yield return (object) 0.2f;
+                    List<Image>.Enumerator enumerator2 = images.GetEnumerator();
+                    try {
+                        while (enumerator2.MoveNext()) {
+                            Image img = enumerator2.Current;
+                            SceneAs<Level>().Particles.Emit(P_Crumble, 2, Position + img.Position + new Vector2(0f, 2f), Vector2.One * 3f);
+                        }
+                    } finally {
+                        ((IDisposable) enumerator2).Dispose();
+                    }
+                    enumerator2 = default(List<Image>.Enumerator);
+                }
+                float timer = crumbleTime;
+                if (!onTop) {
+                    while (timer > 0f) {
+                        yield return (object) null;
+                        timer -= Engine.DeltaTime;
+                    }
+                } else {
+                    while (timer > 0f && GetPlayerOnTop() != null) {
+                        yield return (object) null;
+                        timer -= Engine.DeltaTime;
+                    }
+                }
+                outlineFader.Replace(OutlineFade(1f));
+                occluder.Visible = false;
+                Collidable = false;
+                float delay = 0.05f;
+                for (int l = 0; l < 4; l++) {
+                    for (int i = 0; i < images.Count; i++) {
+                        if (i % 4 - l == 0) {
+                            falls[i].Replace(TileOut(images[fallOrder[i]], delay * (float) l));
+                        }
+                    }
+                }
+                yield return (object) (1.6f + crumbleTime);
+                while (CollideCheck<Actor>() || CollideCheck<Solid>()) {
+                    yield return (object) null;
+                }
+                outlineFader.Replace(OutlineFade(0f));
+                occluder.Visible = true;
+                Collidable = true;
+                for (int k = 0; k < 4; k++) {
+                    for (int j = 0; j < images.Count; j++) {
+                        if (j % 4 - k == 0) {
+                            falls[j].Replace(TileIn(j, images[fallOrder[j]], 0.05f * (float) k));
+                        }
+                    }
+                }
+            }
+        }
+
+        private IEnumerator OutlineFade(float to) {
+            float from = 1f - to;
+            for (float t = 0f; t < 1f; t += Engine.DeltaTime * 2f) {
+                Color color = Color.White * (from + (to - from) * Ease.CubeInOut(t));
+                List<Image>.Enumerator enumerator = outline.GetEnumerator();
+                try {
+                    while (enumerator.MoveNext()) {
+                        Image img = enumerator.Current;
+                        img.Color = color;
+                    }
+                } finally {
+                    ((IDisposable) enumerator).Dispose();
+                }
+                enumerator = default(List<Image>.Enumerator);
+                yield return (object) null;
+            }
+        }
+
+        private IEnumerator TileOut(Image img, float delay) {
+            img.Color = Color.Gray;
+            yield return (object) delay;
+            float distance = (img.X * 7f % 3f + 1f) * 12f;
+            Vector2 from = img.Position;
+            for (float time = 0f; time < 1f; time += Engine.DeltaTime / 0.4f) {
+                yield return (object) null;
+                img.Position = from + Vector2.UnitY * Ease.CubeIn(time) * distance;
+                img.Color = Color.Gray * (1f - time);
+                img.Scale = Vector2.One * (1f - time * 0.5f);
+            }
+            img.Visible = false;
+        }
+
+        private IEnumerator TileIn(int index, Image img, float delay) {
+            yield return (object) delay;
+            Audio.Play("event:/game/general/platform_return", Center);
+            img.Visible = true;
+            img.Color = Color.White;
+            img.Position = new Vector2((float) (index * 8 + 4), 4f);
+            for (float time = 0f; time < 1f; time += Engine.DeltaTime / 0.25f) {
+                yield return (object) null;
+                img.Scale = Vector2.One * (1f + Ease.BounceOut(1f - time) * 0.2f);
+            }
+            img.Scale = Vector2.One;
+        }
+
+        public void orig_Added(Scene scene) {
+            base.Added(scene);
+            MTexture mTexture = GFX.Game["objects/crumbleBlock/outline"];
+            outline = new List<Image>();
+            if (base.Width <= 8f) {
+                Image image = new Image(mTexture.GetSubtexture(24, 0, 8, 8, null));
+                image.Color = Color.White * 0f;
+                Add(image);
+                outline.Add(image);
+            } else {
+                for (int i = 0; (float) i < base.Width; i += 8) {
+                    int num = (i != 0) ? ((i > 0 && (float) i < base.Width - 8f) ? 1 : 2) : 0;
+                    Image image2 = new Image(mTexture.GetSubtexture(num * 8, 0, 8, 8, null));
+                    image2.Position = new Vector2((float) i, 0f);
+                    image2.Color = Color.White * 0f;
+                    Add(image2);
+                    outline.Add(image2);
+                }
+            }
+            Add(outlineFader = new Coroutine(true));
+            outlineFader.RemoveOnComplete = false;
+            images = new List<Image>();
+            falls = new List<Coroutine>();
+            fallOrder = new List<int>();
+            MTexture mTexture2 = GFX.Game["objects/crumbleBlock/" + AreaData.Get(scene).CrumbleBlock];
+            for (int j = 0; (float) j < base.Width; j += 8) {
+                int num2 = (int) ((Math.Abs(base.X) + (float) j) / 8f) % 4;
+                Image image3 = new Image(mTexture2.GetSubtexture(num2 * 8, 0, 8, 8, null));
+                image3.Position = new Vector2((float) (4 + j), 4f);
+                image3.CenterOrigin();
+                Add(image3);
+                images.Add(image3);
+                Coroutine coroutine = new Coroutine(true);
+                coroutine.RemoveOnComplete = false;
+                falls.Add(coroutine);
+                Add(coroutine);
+                fallOrder.Add(j / 8);
+            }
+            fallOrder.Shuffle();
+            Add(new Coroutine(Sequence(), true));
+            Add(shaker = new ShakerList(images.Count, false, delegate (Vector2[] v) {
+                for (int k = 0; k < images.Count; k++) {
+                    images[k].Position = new Vector2((float) (4 + k * 8), 4f) + v[k];
+                }
+            }));
+            Add(occluder = new LightOcclude(0.2f));
+        }
+    }
+}
