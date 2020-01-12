8caabe9f68126e77b62c6deb29aa5be0f57f9576
PenguinOwl <henryjboles@gmail.com>
2020-01-11 18:01:42 -0800

    Clean up
    Just a few things here and there

--- Entities/VariableCrumbleBlock.cs
+++ Entities/VariableCrumbleBlock.cs
@@ -58,46 +58,32 @@ namespace Celeste.Mod.SpringCollab2020.Entities {
                 } else {
                     player = GetPlayerClimbing();
                     if (player == null) {
-                        yield return (object) null;
+                        yield return null;
                         continue;
                     }
                     onTop = false;
                     Input.Rumble(RumbleStrength.Medium, RumbleLength.Medium);
                 }
                 Audio.Play("event:/game/general/platform_disintegrate", Center);
-                shaker.ShakeFor(onTop ? crumbleTime + 0.1f : crumbleTime + 0.2f, false);
-                List<Image>.Enumerator enumerator = images.GetEnumerator();
-                try {
-                    while (enumerator.MoveNext()) {
-                        Image img2 = enumerator.Current;
-                        SceneAs<Level>().Particles.Emit(P_Crumble, 2, Position + img2.Position + new Vector2(0f, 2f), Vector2.One * 3f);
-                    }
-                } finally {
-                    ((IDisposable) enumerator).Dispose();
+                shaker.ShakeFor(onTop ? crumbleTime + 0.4f : crumbleTime + 0.8f, false);
+                foreach (Image img2 in images) {
+                    SceneAs<Level>().Particles.Emit(P_Crumble, 2, Position + img2.Position + new Vector2(0f, 2f), Vector2.One * 3f);
                 }
-                enumerator = default(List<Image>.Enumerator);
-                for (int m = 0; m < (onTop ? 1 : 3); m++) {
-                    yield return (object) 0.2f;
-                    List<Image>.Enumerator enumerator2 = images.GetEnumerator();
-                    try {
-                        while (enumerator2.MoveNext()) {
-                            Image img = enumerator2.Current;
-                            SceneAs<Level>().Particles.Emit(P_Crumble, 2, Position + img.Position + new Vector2(0f, 2f), Vector2.One * 3f);
-                        }
-                    } finally {
-                        ((IDisposable) enumerator2).Dispose();
+                for (int l = 0; l < (onTop ? 1 : 3); l++) {
+                    yield return 0.2f;
+                    foreach (Image img in images) {
+                        SceneAs<Level>().Particles.Emit(P_Crumble, 2, Position + img.Position + new Vector2(0f, 2f), Vector2.One * 3f);
                     }
-                    enumerator2 = default(List<Image>.Enumerator);
                 }
                 float timer = crumbleTime;
-                if (!onTop) {
-                    while (timer > 0f) {
-                        yield return (object) null;
+                if (onTop) {
+                    while (timer > 0f && GetPlayerOnTop() != null) {
+                        yield return null;
                         timer -= Engine.DeltaTime;
                     }
                 } else {
-                    while (timer > 0f && GetPlayerOnTop() != null) {
-                        yield return (object) null;
+                    while (timer > 0f) {
+                        yield return null;
                         timer -= Engine.DeltaTime;
                     }
                 }
@@ -105,16 +91,16 @@ namespace Celeste.Mod.SpringCollab2020.Entities {
                 occluder.Visible = false;
                 Collidable = false;
                 float delay = 0.05f;
-                for (int l = 0; l < 4; l++) {
+                for (int m = 0; m < 4; m++) {
                     for (int i = 0; i < images.Count; i++) {
-                        if (i % 4 - l == 0) {
-                            falls[i].Replace(TileOut(images[fallOrder[i]], delay * (float) l));
+                        if (i % 4 - m == 0) {
+                            falls[i].Replace(TileOut(images[fallOrder[i]], delay * (float) m));
                         }
                     }
                 }
-                yield return (object) (1.6f + crumbleTime);
+                yield return 2f;
                 while (CollideCheck<Actor>() || CollideCheck<Solid>()) {
-                    yield return (object) null;
+                    yield return null;
                 }
                 outlineFader.Replace(OutlineFade(0f));
                 occluder.Visible = true;
@@ -133,27 +119,20 @@ namespace Celeste.Mod.SpringCollab2020.Entities {
             float from = 1f - to;
             for (float t = 0f; t < 1f; t += Engine.DeltaTime * 2f) {
                 Color color = Color.White * (from + (to - from) * Ease.CubeInOut(t));
-                List<Image>.Enumerator enumerator = outline.GetEnumerator();
-                try {
-                    while (enumerator.MoveNext()) {
-                        Image img = enumerator.Current;
-                        img.Color = color;
-                    }
-                } finally {
-                    ((IDisposable) enumerator).Dispose();
+                foreach (Image img in outline) {
+                    img.Color = color;
                 }
-                enumerator = default(List<Image>.Enumerator);
-                yield return (object) null;
+                yield return null;
             }
         }
 
         private IEnumerator TileOut(Image img, float delay) {
             img.Color = Color.Gray;
-            yield return (object) delay;
+            yield return delay;
             float distance = (img.X * 7f % 3f + 1f) * 12f;
             Vector2 from = img.Position;
             for (float time = 0f; time < 1f; time += Engine.DeltaTime / 0.4f) {
-                yield return (object) null;
+                yield return null;
                 img.Position = from + Vector2.UnitY * Ease.CubeIn(time) * distance;
                 img.Color = Color.Gray * (1f - time);
                 img.Scale = Vector2.One * (1f - time * 0.5f);
@@ -162,13 +141,13 @@ namespace Celeste.Mod.SpringCollab2020.Entities {
         }
 
         private IEnumerator TileIn(int index, Image img, float delay) {
-            yield return (object) delay;
+            yield return delay;
             Audio.Play("event:/game/general/platform_return", Center);
             img.Visible = true;
             img.Color = Color.White;
-            img.Position = new Vector2((float) (index * 8 + 4), 4f);
+            img.Position = new Vector2(index * 8 + 4, 4f);
             for (float time = 0f; time < 1f; time += Engine.DeltaTime / 0.25f) {
-                yield return (object) null;
+                yield return null;
                 img.Scale = Vector2.One * (1f + Ease.BounceOut(1f - time) * 0.2f);
             }
             img.Scale = Vector2.One;
@@ -179,21 +158,21 @@ namespace Celeste.Mod.SpringCollab2020.Entities {
             MTexture mTexture = GFX.Game["objects/crumbleBlock/outline"];
             outline = new List<Image>();
             if (base.Width <= 8f) {
-                Image image = new Image(mTexture.GetSubtexture(24, 0, 8, 8, null));
+                Image image = new Image(mTexture.GetSubtexture(24, 0, 8, 8));
                 image.Color = Color.White * 0f;
                 Add(image);
                 outline.Add(image);
             } else {
                 for (int i = 0; (float) i < base.Width; i += 8) {
                     int num = (i != 0) ? ((i > 0 && (float) i < base.Width - 8f) ? 1 : 2) : 0;
-                    Image image2 = new Image(mTexture.GetSubtexture(num * 8, 0, 8, 8, null));
-                    image2.Position = new Vector2((float) i, 0f);
+                    Image image2 = new Image(mTexture.GetSubtexture(num * 8, 0, 8, 8));
+                    image2.Position = new Vector2(i, 0f);
                     image2.Color = Color.White * 0f;
                     Add(image2);
                     outline.Add(image2);
                 }
             }
-            Add(outlineFader = new Coroutine(true));
+            Add(outlineFader = new Coroutine());
             outlineFader.RemoveOnComplete = false;
             images = new List<Image>();
             falls = new List<Coroutine>();
@@ -201,22 +180,22 @@ namespace Celeste.Mod.SpringCollab2020.Entities {
             MTexture mTexture2 = GFX.Game["objects/crumbleBlock/" + AreaData.Get(scene).CrumbleBlock];
             for (int j = 0; (float) j < base.Width; j += 8) {
                 int num2 = (int) ((Math.Abs(base.X) + (float) j) / 8f) % 4;
-                Image image3 = new Image(mTexture2.GetSubtexture(num2 * 8, 0, 8, 8, null));
-                image3.Position = new Vector2((float) (4 + j), 4f);
+                Image image3 = new Image(mTexture2.GetSubtexture(num2 * 8, 0, 8, 8));
+                image3.Position = new Vector2(4 + j, 4f);
                 image3.CenterOrigin();
                 Add(image3);
                 images.Add(image3);
-                Coroutine coroutine = new Coroutine(true);
+                Coroutine coroutine = new Coroutine();
                 coroutine.RemoveOnComplete = false;
                 falls.Add(coroutine);
                 Add(coroutine);
                 fallOrder.Add(j / 8);
             }
             fallOrder.Shuffle();
-            Add(new Coroutine(Sequence(), true));
+            Add(new Coroutine(Sequence()));
             Add(shaker = new ShakerList(images.Count, false, delegate (Vector2[] v) {
                 for (int k = 0; k < images.Count; k++) {
-                    images[k].Position = new Vector2((float) (4 + k * 8), 4f) + v[k];
+                    images[k].Position = new Vector2(4 + k * 8, 4f) + v[k];
                 }
             }));
             Add(occluder = new LightOcclude(0.2f));
