26ceb46b8a1441740d9a78eee4805be0d25c49c4
max480 <max480@free.fr>
2020-02-18 13:32:38 +0100

    Fix Custom Sandwich Lava Settings Trigger in Ahorn (Number property)

--- Ahorn/triggers/customSandwichLavaSettings.jl
+++ Ahorn/triggers/customSandwichLavaSettings.jl
@@ -3,7 +3,7 @@ module SpringCollab2020CustomSandwichLavaSettingsTrigger
 using ..Ahorn, Maple
 
 @mapdef Trigger "SpringCollab2020/CustomSandwichLavaSettingsTrigger" CustomSandwichLavaSettingsTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight,
-    onlyOnce::Bool=false, direction::String="CoreModeBased", speed::Number=20)
+    onlyOnce::Bool=false, direction::String="CoreModeBased", speed::Number=20.0)
 
 const directions = String["AlwaysUp", "AlwaysDown", "CoreModeBased"]
 
