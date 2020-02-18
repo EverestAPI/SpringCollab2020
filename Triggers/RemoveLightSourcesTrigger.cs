04b9a3ea1e1ad5ad50b78976e3c679f3baa7a0c5
WoofWoofDoggo <barkbarkpupper@gmail.com>
2020-02-18 16:40:38 -0500

    Fix remove light source trigger

--- Triggers/RemoveLightSourcesTrigger.cs
+++ Triggers/RemoveLightSourcesTrigger.cs
@@ -1,14 +1,12 @@
 ﻿using Celeste.Mod.Entities;
 using Monocle;
 using Microsoft.Xna.Framework;
-using System.Collections.Generic;
 
 namespace Celeste.Mod.SpringCollab2020.Triggers {
     [CustomEntity("SpringCollab2020/RemoveLightSourcesTrigger")]
     [Tracked]
     class RemoveLightSourcesTrigger : Trigger {
         public RemoveLightSourcesTrigger(EntityData data, Vector2 offset) : base(data, offset) {
-            Persistent = data.Bool("persistent", true);
             level = SceneAs<Level>();
         }
 
@@ -23,40 +21,26 @@ namespace Celeste.Mod.SpringCollab2020.Triggers {
         }
 
         private static void LevelLoadHandler(Level loadedLevel, Player.IntroTypes playerIntro, bool isFromLoader) {
-            if (loadedLevel.Session.GetFlag("lightsDisabled")) {
-                DisableAllLights(loadedLevel);
-                On.Celeste.Level.TransitionTo += TransitionLightSources;
-            }
+            if (loadedLevel.Session.GetFlag("lightsDisabled"))
+                DisableLightRender();
         }
 
         private static void OnExitHandler(Level exitLevel, LevelExit exit, LevelExit.Mode mode, Session session, HiresSnow snow) {
-            On.Celeste.Level.TransitionTo -= TransitionLightSources;
+            EnableLightRender();
         }
 
-        private static void TransitionLightSources(On.Celeste.Level.orig_TransitionTo orig, Level transitionLevel, LevelData next, Vector2 direction) {
-            lightSources = new List<Component>();
-            bloomSources = new List<Component>();
+        private static void LightRendererHook(On.Celeste.LightingRenderer.orig_Render orig, LightingRenderer self, Scene scene) { }
 
-            DisableAllLights(transitionLevel);
-            orig(transitionLevel, next, direction);
+        private static void BloomRendererHook(On.Celeste.BloomRenderer.orig_Apply orig, BloomRenderer self, VirtualRenderTarget target, Scene scene) { }
+
+        private static void EnableLightRender() {
+            On.Celeste.LightingRenderer.Render -= LightRendererHook;
+            On.Celeste.BloomRenderer.Apply -= BloomRendererHook;
         }
 
-        private static void DisableAllLights(Level disableLevel) {
-            EntityList entities = disableLevel.Entities;
-
-            foreach (Entity entity in entities) {
-                foreach (Component component in entity.Components.ToArray()) {
-                    if (component is VertexLight) {
-                        lightSources.Add(component);
-                        component.Visible = false;
-                    }
-
-                    if (component is BloomPoint) {
-                        bloomSources.Add(component);
-                        component.Visible = false;
-                    }
-                }
-            }
+        private static void DisableLightRender() {
+            On.Celeste.LightingRenderer.Render += LightRendererHook;
+            On.Celeste.BloomRenderer.Apply += BloomRendererHook;
         }
 
         public override void OnEnter(Player player) {
@@ -64,34 +48,12 @@ namespace Celeste.Mod.SpringCollab2020.Triggers {
 
             level = SceneAs<Level>();
 
-            if (Persistent && level.Session.GetFlag("lightsDisabled") == false)
-                On.Celeste.Level.TransitionTo += TransitionLightSources;
-
-            if (Persistent)
+            if (level.Session.GetFlag("lightsDisabled") == false) {
                 level.Session.SetFlag("lightsDisabled", true);
-
-            DisableAllLights(level);
+                DisableLightRender();
+            }
         }
-
-        public override void OnLeave(Player player) {
-            base.OnLeave(player);
-
-            if (Persistent || level.Session.GetFlag("lightsDisabled") == true)
-                return;
-
-            foreach (Component component in lightSources)
-                component.Visible = true;
-
-            foreach (Component component in bloomSources)
-                component.Visible = true;
-        }
-
-        private static List<Component> lightSources = new List<Component>();
-
-        private static List<Component> bloomSources = new List<Component>();
 
         private Level level;
-
-        private bool Persistent = true;
     }
 }
