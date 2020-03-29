081933336acda2b396bf43ba5ab790164ddaf062
max480 <max480@free.fr>
2020-03-29 22:36:10 +0200

    Summit checkpoints with negative numbers (... yes)

---
+++ Entities/NegativeSummitCheckpoint.cs
@@ -0,0 +1,22 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using MonoMod.Utils;
+
+namespace Celeste.Mod.SpringCollab2020.Entities {
+    [CustomEntity("SpringCollab2020/NegativeSummitCheckpoint")]
+    class NegativeSummitCheckpoint : SummitCheckpoint {
+        public NegativeSummitCheckpoint(EntityData data, Vector2 offset) : base(data, offset) {
+            // convert the checkpoint number to string. the minus sign is replaced by :, because this is the character after 9 in the chartable.
+            // so, : will render the number10.png sprite, which is a minus sign.
+            string numberString = Number.ToString().Replace("-", ":");
+
+            // add leading zeroes
+            while (numberString.Length < 2) {
+                numberString = $"0{numberString}";
+            }
+
+            // replace the vanilla number string, which will be rendered.
+            new DynData<SummitCheckpoint>(this)["numberString"] = numberString;
+        }
+    }
+}
