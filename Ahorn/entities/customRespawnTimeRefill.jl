a4179b312ebe3f8ebe85060cba3726c552b02e73
max480 <max480@free.fr>
2020-04-12 11:23:32 +0200

    Refill with custom respawn time

---
+++ Ahorn/entities/customRespawnTimeRefill.jl
@@ -0,0 +1,42 @@
+﻿module SpringCollab2020CustomRespawnTimeRefill
+
+using ..Ahorn, Maple
+
+@mapdef Entity "SpringCollab2020/CustomRespawnTimeRefill" CustomRespawnTimeRefill(x::Integer, y::Integer, twoDash::Bool=false, respawnTime::Number=2.5)
+
+const placements = Ahorn.PlacementDict(
+    "Refill (Custom Respawn Time) (Spring Collab 2020)" => Ahorn.EntityPlacement(
+        CustomRespawnTimeRefill
+    ),
+
+    "Refill (Two Dashes, Custom Respawn Time) (Spring Collab 2020)" => Ahorn.EntityPlacement(
+        CustomRespawnTimeRefill,
+        "point",
+        Dict{String, Any}(
+            "twoDash" => true
+        )
+    )
+)
+
+spriteOneDash = "objects/refill/idle00"
+spriteTwoDash = "objects/refillTwo/idle00"
+
+function getSprite(entity::CustomRespawnTimeRefill)
+    twoDash = get(entity.data, "twoDash", false)
+
+    return twoDash ? spriteTwoDash : spriteOneDash
+end
+
+function Ahorn.selection(entity::CustomRespawnTimeRefill)
+    x, y = Ahorn.position(entity)
+    sprite = getSprite(entity)
+
+    return Ahorn.getSpriteRectangle(sprite, x, y)
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::CustomRespawnTimeRefill, room::Maple.Room)
+    sprite = getSprite(entity)
+    Ahorn.drawSprite(ctx, sprite, 0, 0)
+end
+
+end
---
+++ Entities/CustomRespawnTimeRefill.cs
@@ -0,0 +1,28 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+using System;
+
+namespace Celeste.Mod.SpringCollab2020.Entities {
+    [CustomEntity("SpringCollab2020/CustomRespawnTimeRefill")]
+    class CustomRespawnTimeRefill : Refill {
+        public CustomRespawnTimeRefill(EntityData data, Vector2 offset) : base(data, offset) {
+            DynData<Refill> self = new DynData<Refill>(this);
+            float respawnTime = data.Float("respawnTime", 2.5f);
+
+            foreach (Component component in this) {
+                if (component is PlayerCollider collider) {
+                    // wrap the original OnPlayer method to modify the respawnTimer if it gets reset to 2.5f.
+                    Action<Player> orig = collider.OnCollide;
+                    collider.OnCollide = player => {
+                        orig(player);
+                        if (self.Get<float>("respawnTimer") == 2.5f) {
+                            self["respawnTimer"] = respawnTime;
+                        }
+                    };
+                }
+            }
+        }
+    }
+}
