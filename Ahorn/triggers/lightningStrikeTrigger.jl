dcf352a0436807b692ed4be805922364b0692588
WoofWoofDoggo <barkbarkpupper@gmail.com>
2020-04-24 16:18:05 -0400

    Lightning strike additions

--- Ahorn/triggers/lightningStrikeTrigger.jl
+++ Ahorn/triggers/lightningStrikeTrigger.jl
@@ -2,7 +2,7 @@
 
 using ..Ahorn, Maple
 
-@mapdef Trigger "SpringCollab2020/LightningStrikeTrigger" LightningStrikeTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight, playerOffset::Number=0.0, seed::Integer=0, delay::Number=0.0, rain::Bool=true, flash::Bool=true)
+@mapdef Trigger "SpringCollab2020/LightningStrikeTrigger" LightningStrikeTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight, playerOffset::Number=0.0, verticalOffset::Number=0.0, strikeHeight::Number=0.0, seed::Integer=0, delay::Number=0.0, rain::Bool=true, flash::Bool=true, constant::Bool=false)
 
 const placements = Ahorn.PlacementDict(
     "Lightning Strike (Spring Collab 2020)" => Ahorn.EntityPlacement(
