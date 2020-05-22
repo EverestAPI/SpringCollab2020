b47ba5907db1b2cb79455220f8780253280f7189
max480 <max480@free.fr>
2020-05-22 22:09:54 +0200

    Remove non existing parameter from Ahorn plugin

--- Ahorn/triggers/leaveTheoBehindTrigger.jl
+++ Ahorn/triggers/leaveTheoBehindTrigger.jl
@@ -2,8 +2,7 @@ module SpringCollab2020LeaveTheoBehindTrigger
 
 using ..Ahorn, Maple
 
-@mapdef Trigger "SpringCollab2020/LeaveTheoBehindTrigger" LeaveTheoBehindTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight,
-    enable::Bool=false)
+@mapdef Trigger "SpringCollab2020/LeaveTheoBehindTrigger" LeaveTheoBehindTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight)
 
 const placements = Ahorn.PlacementDict(
     "Leave Theo Behind (Spring Collab 2020)" => Ahorn.EntityPlacement(
