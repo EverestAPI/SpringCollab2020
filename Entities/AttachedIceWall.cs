0c3d60e64883911f612919b4ccc79ecebcf88642
max4805 <max480@free.fr>
2021-02-11 09:38:59 +0100

    Fix Attached Ice Wall depth

--- Entities/AttachedIceWall.cs
+++ Entities/AttachedIceWall.cs
@@ -11,6 +11,7 @@ namespace Celeste.Mod.SpringCollab2020.Entities {
             WallBooster iceWall = new WallBooster(entityData.Position + offset, entityData.Height, left, notCoreMode: true);
             StaticMover staticMover = iceWall.Get<StaticMover>();
             staticMover.SolidChecker = solid => iceWall.CollideCheck(solid, iceWall.Position + (left ? -2 : 2) * Vector2.UnitX);
+            staticMover.OnAttach = platform => iceWall.Depth = platform.Depth + 1;
             iceWall.Get<CoreModeListener>().RemoveSelf();
             return iceWall;
         }
